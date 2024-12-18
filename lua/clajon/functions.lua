c6f2054a6d5cd11067af6bcea778dae126b57421
Clapton Jonsson <clapton.jonsson@outlook.com>
2024-12-18 15:07:57 -0300

    a

--- lua/clajon/functions.lua
+++ lua/clajon/functions.lua
@@ -1,16 +1,14 @@
--- Lua function to perform different actions based on filetype
+-- TODO: Make this into a code format file only!
 local function handle_filetype()
-    -- Get the filetype of the current buffer
-    local ft = vim.bo.filetype
-    -- Check filetype and perform actions accordingly
-    if ft == "html" or ft == "css" or ft == "javascript" then
-          vim.cmd("!prettier --write " .. vim.fn.expand("%"))
-
-    elseif ft == "lua" then
-        vim.cmd("echo 'This is Lua'")
-        -- Add your custom actions for Lua files here
-    end
+	-- Get the filetype of the current buffer
+	local ft = vim.bo.filetype
+	-- Check filetype and perform actions accordingly
+	if ft == "html" or ft == "css" or ft == "javascript" then
+		vim.cmd("!prettier --write " .. vim.fn.expand("%"))
+	elseif ft == "lua" then
+		vim.cmd("!stylua " .. vim.fn.expand("%"))
+	end
 end
 
 -- Register the function as a user command (make it callable with :HandleFiletype)
-vim.api.nvim_create_user_command('HandleFiletype', handle_filetype, {})
+vim.api.nvim_create_user_command("HandleFiletype", handle_filetype, {})
--- lua/clajon/remap.lua
+++ lua/clajon/remap.lua
@@ -4,7 +4,11 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 vim.keymap.set("n", "<C-s>", vim.cmd.wa)
-vim.keymap.set("n", "<leader>cf", function() vim.cmd("HandleFiletype") end, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>g", vim.cmd.Git)
 vim.keymap.set("n", "<leader>j", vim.cmd.bn)
 vim.keymap.set("n", "<leader>k", vim.cmd.bp)
+
+
+--plugins
+vim.keymap.set("n", "<leader>cf", function() vim.cmd("HandleFiletype") end, { noremap = true, silent = true })
+
