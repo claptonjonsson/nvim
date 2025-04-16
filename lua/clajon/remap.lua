0c23f90629ee01328d03721a8006a20185215e3d
Clapton Jonsson <clapton.jonsson@outlook.com>
2025-04-15 22:44:40 -0300

    added bindings for accepting words and lines of Copilot suggestions

--- lua/clajon/remap.lua
+++ lua/clajon/remap.lua
@@ -6,7 +6,7 @@ vim.g.maplocalleader = "\\"
 
 --vim.keymap.set("n", "<C-s>", vim.cmd.wa)
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex) -- File browser
-vim.keymap.set('n', 'H', ':noh<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "H", ":noh<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>g", vim.cmd.Git) -- Telescope Git
 
@@ -19,5 +19,9 @@ vim.keymap.set("n", "<C-s>", function()
 	vim.cmd("OnSave")
 end, { noremap = true, silent = true })
 
---plugins
-vim.keymap.set({ "n", "v" }, "<leader>c", ":CopilotChat ", { noremap = true, silent = true })
+--PLUGINS
+
+--Copilot
+vim.keymap.set({ "n", "v" }, "<leader>c", ":CopilotChat ", { noremap = true, silent = true }) -- Copilot chat
+vim.keymap.set("i", "<S-Tab>", "<Plug>(copilot-accept-word)", { silent = true }) -- Map Ctrl-Tab to accept word
+vim.keymap.set("i", "<C-Tab>", "<Plug>(copilot-accept-line)", { silent = true }) -- Map Shift-Tab to accept line
