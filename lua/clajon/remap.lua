621bb4a16347695b85ae529dbc6f9f269ac3565d
claptonjonsson <clapton.jonsson@outlook.com>
2026-02-28 15:14:39 -0300

    Slight comment change

--- lua/clajon/remap.lua
+++ lua/clajon/remap.lua
@@ -27,5 +27,5 @@ end, { noremap = true, silent = true })
 vim.keymap.set({ "n" }, "<leader>cc", ":CopilotChat ", { noremap = true, silent = true }) -- Copilot chat
 vim.keymap.set({ "n" }, "<leader>ce", ":Copilot enable<CR>", { noremap = true, silent = true }) -- Copilot inline suggestion enable
 vim.keymap.set({ "n" }, "<leader>cd", ":Copilot disable<CR>", { noremap = true, silent = true }) -- Copilot inline suggestion disable
-vim.keymap.set("i", "<S-Tab>", "<Plug>(copilot-accept-word)", { silent = true }) -- Map Ctrl-Tab to accept word
-vim.keymap.set("i", "<A-Tab>", "<Plug>(copilot-accept-line)", { silent = true }) -- Map Shift-Tab to accept line
+vim.keymap.set("i", "<S-Tab>", "<Plug>(copilot-accept-word)", { silent = true }) -- Map Shift-Tab to accept word
+vim.keymap.set("i", "<A-Tab>", "<Plug>(copilot-accept-line)", { silent = true }) -- Map Alt-Tab to accept line
