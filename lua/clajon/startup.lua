7c4cf07496691f13b9ddcd1d749f50acf73717a2
claptonjonsson <clapton.jonsson@outlook.com>
2026-03-06 16:19:24 -0300

    Added a conditional tab for Harlequin in case the project is Cloudflare based.

--- lua/clajon/startup.lua
+++ lua/clajon/startup.lua
@@ -17,6 +17,14 @@ function M.setup()
 			vim.cmd("tabnew")
 			vim.cmd("terminal lazygit")
 
+			--Create a fifth tab for Harlequin
+			if vim.fn.filereadable("wrangler.jsonc") == 1 then
+				vim.cmd("tabnew")
+				vim.cmd(
+					'terminal bash -c \'cd .wrangler/state/v3/d1/miniflare-D1DatabaseObject/ && f=$(ls *.sqlite | head -n1) && [ -n "$f" ] && harlequin -t nord "$f" || echo "No .sqlite file found"\''
+				)
+			end
+
 			-- Return to first tab (main editor window)
 			vim.cmd("tabnext 1")
 		end
